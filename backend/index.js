480d60091fa7e44622f3bc6580cff7a525336e0d
Matthew Tobino <tobino68@students.rowan.edu>
2024-08-22 19:13:20 -0400

    Split should be fully functional, documentation of methods complete

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,10 @@
  */
 const axios = require('axios');
 
+/**?
+ * Player First Turn possible Actions
+ * @type {string}
+ */
 const PLAYER_STANDS = 'PLAYER_STANDS';
 const PLAYER_DOUBLE_DOWNS = 'PLAYER_DOUBLE_DOWN';
 const PLAYER_SPLITS = 'PLAYER_SPLITS';
@@ -64,38 +68,19 @@ function displayResults(player, dealer){
     if(player.blackjack && dealer.blackjack){
         console.log("Both hit blackjack: Push!");
         displayHandResults(player.hand[0].handValue, dealer.hand.handValue);
+        return;
     }
     else if(player.blackjack && !dealer.blackjack){
         console.log("Player wins with 21: blackjack!");
         displayHandResults(player.hand[0].handValue, dealer.hand.handValue);
+        return;
     }
     else if(dealer.blackjack && !player.blackjack){
         console.log("Dealer wins with 21: blackjack :(");
         displayHandResults(player.hand[0].handValue, dealer.hand.handValue);
+        return;
     }
 
-    //
-    //
-    // else if(dealer.hand.bust){
-    //     console.log("Player wins, Dealer busted!");
-    // }
-    // else if(player.hand[0].bust){
-    //     console.log("Player busted :(");
-    // }
-    // else if(player.hand[0].stand && dealer.hand.stand){
-    //
-    //     let playerMax = Math.max(...player.hand[0].handValue.filter(value => value <= 21));
-    //     let dealerMax = Math.max(...dealer.hand.handValue.filter(value => value <= 21));
-    //     if(playerMax > dealerMax){
-    //         console.log(`Player wins with ${playerMax}, dealers has ${dealerMax}`);
-    //     }
-    //     else if(playerMax === dealerMax){
-    //         console.log("Push!")
-    //     }
-    //     else{
-    //         console.log(`Dealer wins with ${dealerMax}, player has ${playerMax}`);
-    //     }
-    // }
     player.hand.forEach((hand, index) =>{
         console.log(`Now checking player hand number ${index + 1}`);
         if(dealer.hand.bust){
@@ -118,19 +103,18 @@ function displayResults(player, dealer){
             }
         }
         displayHandResults(hand.handValue, dealer.hand.handValue);
+        console.log();
     });
 }
+
 function displayHandResults(playerHandValue, dealerHandValue){
     console.log(`Player hand value ${playerHandValue.toString()}`);
     console.log(`Dealer hand value ${dealerHandValue.toString()}`);
 }
 
-
 /**
  * Rudimentary blackjack simulator, grabs a deck, distributes cards, and then runs a sim of what the player
  * would usually do
- *
- * TODO: Add double downs, standing at appropriate times against dealer, splitting
  *
  * @returns {Promise<void>} no returns
  */
@@ -174,11 +158,11 @@ const playRound = async () => {
     switch(playerRecommendedAction){
         case PLAYER_HITS:
             console.log("Player hits normally and will not take any extra actions");
-            await playerPlaysNormally(player, playerFirstHand, dealer, deck_id);
+            await playerHitsUntilTheyCannot(player, playerFirstHand, dealer, deck_id);
             break;
         case PLAYER_DOUBLE_DOWNS:
             console.log("Player is doubling down, good luck");
-            await playerTurn(player, playerFirstHand, dealer, deck_id);
+            await playerHits(player, playerFirstHand, dealer, deck_id);
             break;
         case PLAYER_SPLITS:
             console.log('Player splitting their cards')
@@ -191,36 +175,22 @@ const playRound = async () => {
             console.log("Error: Should not be reaching the default");
             break;
     }
-    console.log();
+    console.log(`\nDealer turns their card to reveal a ${dealer.hand.cards[1].value}`)
     await dealerTurn(player, dealer, deck_id)
     displayResults(player, dealer);
-
-    // while( !player.hand[0].bust && !player.blackjack && !player.hand[0].stand && !dealer.blackjack )
-    // {
-    //
-    //     let newCard = await dealNumberOfCards(deck_id, 1);
-    //     console.log(`Player received a ${newCard[0].value}`);
-    //     player.hand[0].cards.push(newCard[0]);
-    //     updatePlayer(player, dealer);
-    // }
-    //
-    // while( !player.hand[0].bust && !player.blackjack && !dealer.blackjack && !dealer.hand.stand && !dealer.hand.bust){
-    //      let newCard = await dealNumberOfCards(deck_id, 1);
-    //      console.log(`Dealer received a ${newCard[0].value}`);
-    //      dealer.hand.cards.push(newCard[0]);
-    //      updateDealer(dealer)
-    // }
-    // displayResults(player, dealer);
-
 }
+
+/**
+ * Take the player and dealer, update their starting information and display their information
+ * 
+ * @param player    The player
+ * @param dealer    The dealer the player is up against
+ */
 function setupForFirstTurn(player, dealer){
     // Calculate starting hands and positions
     const FIRST_HAND_OR_CARD = 0;
     let playerStartingHand = player.hand[FIRST_HAND_OR_CARD];
     let dealerStartingHand = dealer.hand;
-    // give players their hand values
-    playerStartingHand.handValue = calculatePossibleValues(playerStartingHand.cards);
-    dealerStartingHand.handValue = calculatePossibleValues(dealerStartingHand.cards);
     // adjust player information depending on their hand values
     updatePlayer(player, dealer);
     updateDealer(dealer);
@@ -232,19 +202,46 @@ function setupForFirstTurn(player, dealer){
     console.log(`Dealer starts with: ${dealer.hand.cards[0].value} and a hidden card\n`);
 }
 
-async function playerPlaysNormally(player, handNum, dealer, deck_id){
+/**
+ * Play a round normally, hitting until the player busts, stands, had black jack or the dealer had blackjack
+ * 
+ * @param player                The player
+ * @param handNum               The current hand that is being worked on
+ * @param dealer                The dealer the player is facing
+ * @param deck_id               The deck_id that is needed to call the deck API
+ * @returns {Promise<void>}     No returns
+ */
+async function playerHitsUntilTheyCannot(player, handNum, dealer, deck_id){
     while( !player.hand[handNum].bust && !player.blackjack && !player.hand[handNum].stand && !dealer.blackjack )
     {
-        await playerTurn(player, handNum, dealer, deck_id);
+        await playerHits(player, handNum, dealer, deck_id);
     }
-    console.log();
 }
-async function playerTurn(player, handNum, dealer, deck_id){
+
+/**
+ * Player hits, they get dealt a card and adjust their information based on the new card
+ *
+ * @param player                The player
+ * @param handNum               The current hand of the player
+ * @param dealer                The dealer the player is up against
+ * @param deck_id               The deck_id that is needed to call the deck API
+ * @returns {Promise<void>}     No returns
+ */
+async function playerHits(player, handNum, dealer, deck_id){
     let newCard = await dealNumberOfCards(deck_id, 1);
-    console.log(`Player received a ${newCard[0].value}`);
-    player.hand[0].cards.push(newCard[0]);
-    updatePlayer(player, dealer);
+    console.log(`Player received a ${newCard[0].value} for hand ${handNum + 1}`);
+    player.hand[handNum].cards.push(newCard[0]);
+    updatePlayer(player, dealer, handNum);
 }
+
+/**
+ * Player split their starting cards, and now will play two hands
+ *
+ * @param player                The player
+ * @param dealer                The dealer the player is up against
+ * @param deck_id               The deck_id needed to call the deck API
+ * @returns {Promise<void>}     No returns
+ */
 async function playerSplitTurn(player, dealer, deck_id){
     let newHand = {
         stand: false,
@@ -252,12 +249,29 @@ async function playerSplitTurn(player, dealer, deck_id){
         cards: [],
         handValue:[]
     }
-    newHand.cards.push(player.hand[0].cards.pop());
+    let removedCard = player.hand[0].cards.pop()
+    newHand.cards.push(removedCard);
     player.hand.push(newHand);
-    player.hand.forEach((hand, index) => {
-        playerPlaysNormally(player, index, dealer, deck_id);
-    });
+    let newCards = await dealNumberOfCards(deck_id, 2);
+    player.hand[0].cards.push(newCards[0]);
+    console.log(`Player Received a ${newCards[0].value} for hand 1 to start their new hand`)
+    player.hand[1].cards.push(newCards[1]);
+    console.log(`Player Received a ${newCards[1].value} for hand 2 to start their new hand`)
+    updatePlayer(player, dealer, 0);
+    updatePlayer(player, dealer, 1);
+    for(let handNum = 0; handNum < player.hand.length; handNum++){
+        await playerHitsUntilTheyCannot(player, handNum, dealer, deck_id);
+    }
 }
+
+/**
+ * The dealers turn, they go until they bust, stand, had blackjack at the start, or if the player is fully busted
+ *
+ * @param player            The player
+ * @param dealer            The dealer the player is up against
+ * @param deck_id           The deck_id needed to call the deck API
+ * @returns {Promise<void>} No Returns
+ */
 async function dealerTurn(player, dealer, deck_id){
     while( !player.blackjack && !dealer.blackjack && !dealer.hand.stand && !dealer.hand.bust && !checkIfPlayerFullyBusted(player.hand)){
         let newCard = await dealNumberOfCards(deck_id, 1);
@@ -267,10 +281,23 @@ async function dealerTurn(player, dealer, deck_id){
     }
     console.log();
 }
+
+/**
+ * If the player is busted or not
+ * @param {any[]} playerHands   The hands array of the player
+ * @returns {boolean}           true iff the player has busted on every hand
+ */
 function checkIfPlayerFullyBusted(playerHands){
     return playerHands.every(hand => hand.bust);
 }
 
+/**
+ * Determine the first action the player should take after receiving their cards
+ *
+ * @param player                The player
+ * @param dealer                The dealer the player is up against
+ * @returns {string|string}     The next course of action the player will take
+ */
 function determineFirstTurnAction(player, dealer){
     if(player.blackjack || dealer.blackjack)
     {
@@ -290,6 +317,13 @@ function determineFirstTurnAction(player, dealer){
     return HitSplitOrStand(playerHand, dealer.hand);
 }
 
+/**
+ * Whether the player should double down or not
+ *
+ * @param playerHand    The player's hand
+ * @param dealerHand    The dealer's hand
+ * @returns {boolean}   True iff the player should double down
+ */
 function shouldDoubleDown(playerHand, dealerHand){
     let dealerFaceUpCardValue = getTrueCardValue(dealerHand.cards[0].value);
     let playerHandValue = playerHand.handValue[0];
@@ -303,11 +337,23 @@ function shouldDoubleDown(playerHand, dealerHand){
     else return playerHandValue === 11 && inRange(dealerFaceUpCardValue, 2, 10);
 }
 
-function hasPair(playerHand)
-{
+/**
+ * Determines if the player's hand contains a pair
+ *
+ * @param playerHand    The player's hand
+ * @returns {boolean}   true iff the players first care matches their second
+ */
+function hasPair(playerHand) {
     return playerHand.cards[0].value === playerHand.cards[1].value;
 }
 
+/**
+ * Based on the players current hand, should they split, hit, or stand
+ *
+ * @param playerHand        The players current hand
+ * @param dealerHand        The dealer's hand
+ * @returns {string|string} The player's next course of action
+ */
 function HitSplitOrStand(playerHand, dealerHand){
     if(hasPair(playerHand))
     {
@@ -337,6 +383,13 @@ function HitSplitOrStand(playerHand, dealerHand){
     }
     else return PLAYER_HITS;
 }
+
+/**
+ * Get the true value of the cards
+ *
+ * @param cardValue     The inputted card value
+ * @returns {number}    The card's actual value
+ */
 function getTrueCardValue(cardValue){
     if(cardValue === 'KING' || cardValue === "QUEEN" || cardValue === 'JACK'){
         return 10;
@@ -345,7 +398,7 @@ function getTrueCardValue(cardValue){
         return 11;
     }
     else{
-        return cardValue;
+        return Number(cardValue);
     }
 }
 
@@ -370,7 +423,7 @@ function updateDealer (dealer){
  */
 function updatePlayer (player, dealer, handNum = 0) {
     player.hand[handNum].handValue = calculatePossibleValues(player.hand[handNum].cards);
-    player.hand[handNum].stand = shouldStand(dealer, player);
+    player.hand[handNum].stand = shouldStand(player.hand[handNum].handValue, dealer.hand.cards[0].value);
     player.hand[handNum].bust = player.hand[handNum].handValue.every(value => value > 21);
 }
 /**
@@ -378,17 +431,13 @@ function updatePlayer (player, dealer, handNum = 0) {
  * OR
  * If the player has a soft 17 or higher, Stand
  *
- * @param dealer            The dealer
- * @param player            The player
+ * @param playerHandValues
+ * @param dealerCardValue
  * @returns {boolean}       True, iff the player should stand
  */
-function shouldStand( dealer, player) {
-
-    let handValues = player.hand[0].handValue;
-    let dealerCardValue = dealer.hand.cards[0].value;
-
-    if(isSoft(handValues)){
-        let softMax = Math.max(...handValues);
+function shouldStand(playerHandValues, dealerCardValue) {
+    if(isSoft(playerHandValues)){
+        let softMax = Math.max(...playerHandValues.filter(value => value <= 21));
         if(softMax < 18){
             return false;
         }
@@ -399,13 +448,13 @@ function shouldStand( dealer, player) {
             return true;
         }
     }
-    if(handValues.includes(12)) {
+    if(playerHandValues.includes(12)) {
         return inRange(dealerCardValue, 4, 6);
     }
 
-    let equalToOrOver17 = handValues.filter(value =>  inRange(value, 17, 21) ).length > 0;
+    let equalToOrOver17 = playerHandValues.filter(value =>  inRange(value, 17, 21) ).length > 0;
     let dealerInRange = inRange(dealerCardValue, 2, 6);
-    let playerInRange = handValues.filter(value => inRange(value, 12, 16)).length > 0
+    let playerInRange = playerHandValues.filter(value => inRange(value, 12, 16)).length > 0
     return equalToOrOver17 || (dealerInRange && playerInRange);
 }
 
